ff9c5a52a93540c82ef838beda4d496d930fccab
ktakada42 <ktakada950711@gmail.com>
2022-06-23 16:09:14 +0900

    style: add_comment_to_N2K

--- pkg/server/handler/numbertokanji.go
+++ pkg/server/handler/numbertokanji.go
@@ -17,8 +17,8 @@ const (
 
 var kanjiNumbers = map[int]string{0: "", 1: "壱", 2: "弐", 3: "参", 4: "四", 5: "五", 6: "六", 7: "七", 8: "八", 9: "九"}
 var largeSeparatorsNum = []int{1000000000000, 100000000, 10000, 1} // 1兆, 1億, 1万
-var largeSeparatorsKanji = []string{"兆", "億", "万", ""}
-var smallSeparators = []string{"", "拾", "百", "千"}
+var largeSeparatorsKanji = []string{"兆", "億", "万", ""}             // 桁区切り(大)
+var smallSeparators = []string{"", "拾", "百", "千"}                  // 桁区切り(小)
 
 func HandleNumberToKanji(w http.ResponseWriter, r *http.Request) {
 	u, err := url.Parse(r.URL.Path)
@@ -27,16 +27,26 @@ func HandleNumberToKanji(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	arr := strings.Split(u.Path, "/")
+
+	// パラメーターの数が合わないと204
+	// Ex) /v1/number2kanji/123/123 => 204
+	// Ex) /v1/number2kanji/ => 204
 	if len(arr) != 4 {
 		log.Println("Path invalid")
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
+
+	// 変換できないパラメーターは204
+	// Ex) /v1/number2kanji/a => 204
+	// Ex) /v1/number2kanji/-1 => 204
+	// Ex) /v1/number2kanji/9999999999999999999999999999 => 204
 	if !isPathParamValid(arr[3]) {
 		log.Println("Param invalid")
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
+
 	fmt.Fprintf(w, convertNumberToKanji(arr[3]))
 }
 
@@ -45,6 +55,7 @@ func isPathParamValid(p string) bool {
 	if err != nil {
 		return false
 	}
+	// '+'はURIの予約文字なので弾く
 	if p[0] == '+' {
 		return false
 	}
@@ -60,11 +71,16 @@ func convertNumberToKanji(num string) (kanji string) {
 		kanji = "零"
 		return
 	}
+
+	// パラメーターを上から4桁ずつ配列に入れる
+	// Ex) 123,456,789 => [0(兆), 1(億), 2345(万), 6789]
 	var separatedNum [4]int
 	for i, s := range largeSeparatorsNum {
 		separatedNum[i] = n / s
 		n %= s
 	}
+
+	// 4桁ずつ分けた数字を更に桁区切り(小)で区切る
 	for i, s := range separatedNum {
 		if s != 0 {
 			for i := 3; i >= 0; i-- {
@@ -73,7 +89,7 @@ func convertNumberToKanji(num string) (kanji string) {
 				}
 				s %= int(math.Pow10(i))
 			}
-			kanji += largeSeparatorsKanji[i]
+			kanji += largeSeparatorsKanji[i] // 桁区切り(大)を追加
 		}
 	}
 	return
