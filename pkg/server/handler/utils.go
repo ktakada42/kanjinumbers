c7a6187e51c212fcde19e2cd163b21e67801f63e
ktakada42 <ktakada950711@gmail.com>
2022-06-24 05:11:56 +0900

    chore: rm_extra_utils_source

--- pkg/server/handler/utils.go
+++
@@ -1,9 +0,0 @@
-package handler
-
-func strRev(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
