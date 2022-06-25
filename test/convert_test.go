ed813df772bd852532a61656d84bfb86e472dbc0
ktakada42 <ktakada950711@gmail.com>
2022-06-26 01:37:49 +0900

    test: use_iter

--- test/convert_test.go
+++ test/convert_test.go
@@ -8,37 +8,24 @@ import (
 )
 
 func TestConvertValueCompare(t *testing.T) {
-	t.Run("0 to 9,999", func(t *testing.T) {
+	t.Run("0", func(t *testing.T) {
 		t.Parallel()
-		for i := 0; i <= 9999; i++ {
-			n := strconv.Itoa(i)
-			kanji := handler.ConvertNumberToKanji(n)
-			num, _ := handler.ConvertKanjiToNumber(kanji)
-			if i == num {
-				t.Logf("\x1b[32m\nparameter %d: kanji: %s\n\x1b[0m", i, kanji)
-			} else {
-				t.Fatalf("\x1b[31m\nparameter %d: kanji: %s\n\x1b[0m", i, kanji)
-			}
+		i := 0
+		n := strconv.Itoa(i)
+		kanji := handler.ConvertNumberToKanji(n)
+		num, _ := handler.ConvertKanjiToNumber(kanji)
+		if i == num {
+			t.Logf("\x1b[32m\nparameter %d: kanji: %s\n\x1b[0m", i, kanji)
+		} else {
+			t.Fatalf("\x1b[31m\nparameter %d: kanji: %s\n\x1b[0m", i, kanji)
 		}
 	})
 
-	t.Run("10,000 to 99,990,000", func(t *testing.T) {
-		t.Parallel()
-		for i := 10000; i <= 99990000; i += 10000 {
-			n := strconv.Itoa(i)
-			kanji := handler.ConvertNumberToKanji(n)
-			num, _ := handler.ConvertKanjiToNumber(kanji)
-			if i == num {
-				t.Logf("\x1b[32m\nparameter %d: kanji: %s\n\x1b[0m", i, kanji)
-			} else {
-				t.Fatalf("\x1b[31m\nparameter %d: kanji: %s\n\x1b[0m", i, kanji)
-			}
-		}
-	})
+	for i := 1; i <= 9999; i++ {
+		i := i
 
-	t.Run("100,000,000 to 999,900,000", func(t *testing.T) {
-		t.Parallel()
-		for i := 100000000; i <= 999900000; i += 100000000 {
+		t.Run("1 to 9,999", func(t *testing.T) {
+			t.Parallel()
 			n := strconv.Itoa(i)
 			kanji := handler.ConvertNumberToKanji(n)
 			num, _ := handler.ConvertKanjiToNumber(kanji)
@@ -47,12 +34,11 @@ func TestConvertValueCompare(t *testing.T) {
 			} else {
 				t.Fatalf("\x1b[31m\nparameter %d: kanji: %s\n\x1b[0m", i, kanji)
 			}
-		}
-	})
+		})
 
-	t.Run("1,000,000,000,000 to 9,999,000,000,000,000", func(t *testing.T) {
-		t.Parallel()
-		for i := 1000000000000; i <= 9999000000000000; i += 1000000000000 {
+		t.Run("10,000 to 99,990,000", func(t *testing.T) {
+			t.Parallel()
+			i := i * 10000
 			n := strconv.Itoa(i)
 			kanji := handler.ConvertNumberToKanji(n)
 			num, _ := handler.ConvertKanjiToNumber(kanji)
@@ -61,6 +47,32 @@ func TestConvertValueCompare(t *testing.T) {
 			} else {
 				t.Fatalf("\x1b[31m\nparameter %d: kanji: %s\n\x1b[0m", i, kanji)
 			}
-		}
-	})
+		})
+
+		t.Run("100,000,000 to 999,900,000", func(t *testing.T) {
+			t.Parallel()
+			i := i * 100000000
+			n := strconv.Itoa(i)
+			kanji := handler.ConvertNumberToKanji(n)
+			num, _ := handler.ConvertKanjiToNumber(kanji)
+			if i == num {
+				t.Logf("\x1b[32m\nparameter %d: kanji: %s\n\x1b[0m", i, kanji)
+			} else {
+				t.Fatalf("\x1b[31m\nparameter %d: kanji: %s\n\x1b[0m", i, kanji)
+			}
+		})
+
+		t.Run("1,000,000,000,000 to 9,999,000,000,000,000", func(t *testing.T) {
+			t.Parallel()
+			i := i * 1000000000000
+			n := strconv.Itoa(i)
+			kanji := handler.ConvertNumberToKanji(n)
+			num, _ := handler.ConvertKanjiToNumber(kanji)
+			if i == num {
+				t.Logf("\x1b[32m\nparameter %d: kanji: %s\n\x1b[0m", i, kanji)
+			} else {
+				t.Fatalf("\x1b[31m\nparameter %d: kanji: %s\n\x1b[0m", i, kanji)
+			}
+		})
+	}
 }
